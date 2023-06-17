b842aa26ab26789b92ffa3cf21f3137871fc7977
Peterson Macedo <petersonmacedo.pm@gmail.com>
2023-06-17 16:00:37 -0400

    teste ola mundo

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'testes'),
+      home: const MyHomePage(title: 'ola mundo'),
     );
   }
 }
